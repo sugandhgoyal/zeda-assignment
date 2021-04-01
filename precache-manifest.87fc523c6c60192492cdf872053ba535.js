9e4a43b44063e0d7e3b015bb3ff77ea1b208f4b7
sugandhgoyal <goyalsugandh2@gmail.com>
2021-04-01 21:35:11 +0530

    Updates

---
+++ precache-manifest.87fc523c6c60192492cdf872053ba535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8316984b10e06cfdffdbf06ddaf10bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f6482ee4d0c0a8e78d1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5dd644b49e309be2306",
+    "url": "/static/js/2.cd60bf07.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.cd60bf07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6482ee4d0c0a8e78d1",
+    "url": "/static/js/main.cab8d039.chunk.js"
+  },
+  {
+    "revision": "2c46d6b47a8bafd391ba",
+    "url": "/static/js/runtime-main.64c6a6eb.js"
+  }
+]);
